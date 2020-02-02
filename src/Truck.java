81978b3defd717b01751c5151f439d9cfde75477
bishops250 <biishops250@gmail.com>
2020-02-02 17:02:23 -0500

    Completed problem...

--- src/Truck.java
+++ src/Truck.java
@@ -2,47 +2,98 @@ public class Truck {
     public String truckID;
     private static int object_amount;
     private double odometer;
-    private double mps;
+    private double mpg;
     private double fuel;
     private final double CAPACITY= 20;
     private static double totalFuel;
     private double miles;
 
-    public Truck() {
+    public Truck(String truckID) {
 
+        this.truckID= truckID;
+        this.odometer= 0;
+        this.mpg= 0;
+        this.fuel= 0;
+        totalFuel+=fuel;
     }
 
-    public Truck() {
-
+    public Truck(String truckID,double odometer, double mpg, double fuel) {
+        this.truckID= truckID;
+        this.odometer= odometer;
+        this.mpg= mpg;
+        this.fuel= fuel;
+        totalFuel+=fuel;
     }
 
-    public boolean drive(double miles) {
+    public void setMps(double mpg) {
+        this.mpg = mpg;
+    }
 
+    public String getTruckID() {
+        return truckID;
+    }
+
+    public String drive(double miles) {
+        if(enoughFuel(miles)) {
+            odometer+=miles;
+            fuel -=(miles/mpg);
+
+            return "Success";
+        }
+        else {
+            return "Truck: "+truckID+ " does not have enough fuel to drive "+miles+ " miles.";
+        }
     }
 
     public double getCAPACITY() {
-        return CAPACITY;
+        return this.CAPACITY;
     }
 
-    public double getMps() {
-        return mps;
+    public double getMpg() {
+        return this.mpg;
 
     }
 
     public double getFuel() {
-        return fuel;
+        return this.fuel;
     }
 
     public double getOdometer() {
-        return odometer;
+        return this.odometer;
     }
 
     public static double getTotalFuel() {
         return totalFuel;
     }
 
-    public boolean enoughFuel(double CAPACITY) {
+    public boolean enoughFuel(double miles) {
+       return (this.mpg * this.fuel)>=miles;
+    }
 
+    public double getMiles() {
+        return this.miles;
+    }
+
+    public void fill() {
+        totalFuel += (this.CAPACITY - this.fuel);
+        this.fuel = CAPACITY;
+    }
+
+    public String fill(double gallons) {
+        if(this.fuel +gallons<=this.CAPACITY) {
+            this.fuel += gallons;
+            totalFuel += gallons;
+            return "Success";
+        }
+        else {
+            return "Truck "+ truckID+ ": Gallons exceeds tank capacity.";
+        }
+
+    }
+
+    public String toString() {
+
+        return "Truck: "+truckID+"\n"+"Odometer: "+odometer+"\n"+"Miles Per Gallon:" +mpg+"\n"+"Fuel:"+fuel;
     }
 }
 
---
+++ src/TruckTest.java
@@ -0,0 +1,25 @@
+public class TruckTest {
+
+    public static void main(String[]args) {
+        Truck truck1= new Truck("123ABC");
+        Truck truck2= new Truck("456DEF",45500,18,10);
+
+        truck1.setMps(15);
+        truck1.fill();
+        truck1.drive(150);
+        System.out.println(truck1);
+
+        truck1.drive(60);
+        System.out.println(truck1);
+
+
+        truck2.drive(500);
+        truck2.fill(30);
+        truck2.fill();
+        truck2.drive(300);
+        System.out.println(truck2);
+
+        System.out.println(Truck.getTotalFuel());
+        
+    }
+}
