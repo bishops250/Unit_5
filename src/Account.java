22fef725a739a9e305c7f41e8163ee4cabc91bbd
bishops250 <biishops250@gmail.com>
2020-02-03 20:34:59 -0500

    Corrected.

--- src/Account.java
+++ src/Account.java
@@ -8,9 +8,60 @@ public class Account {
         this.first = first;
         this.last = last;
         this.balance = balance;
-        acctNum=(int)(Math.random()*999)+1;
+        acctNum=(int)(Math.random()*998)+1;
+    }
+
+    public Account() {
+        this("","",0) ;
     }
     public Account(Account dup) {
-        this()
+        this(dup.first,dup.last,dup.balance);
+
+    }
+    public String getFirst() {
+        return first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public int getAcctNum() {
+        return acctNum;
+    }
+
+    public void setFirst(String first) {
+      this.first= first;
+    }
+
+    public void setLast(String last) {
+        this.last= last;
+    }
+
+    public void deposit(double depositAmount) {
+        balance += depositAmount;
+    }
+
+    public boolean withdrawal(double withdrawalAmount) {
+        if(withdrawalAmount<=balance) { /*Is this a potential issue can we get a 0 or unattended number.
+            */
+            balance -= withdrawalAmount;
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+    public String toString() {
+        return "Customer Name: " + first +" " +last + "\n"+
+                "Account #: " +acctNum+ "\n"+
+                "Current Balance: "+"$"+balance;
+
     }
 }
