d0d23dde28e7c52941fe431b1117e5d78178053c
bishops250 <biishops250@gmail.com>
2020-02-09 14:20:24 -0500

    Finally Correct the Error. Added Documentation. Is It an issue that I have the slashes?

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,10 +1,10 @@
 /**
  *                                                  \\//Fraction-Class\\//
- *                                                  
+ *
  * The fraction class is a class that allows the user to complete all basic operations with Fractions. This includes...
  *
- * Addition, Subtraction, Multiplication, & Division
- * Simplification & Reducing Fractions
+ * Addition, Subtraction, Multiplication, and Division
+ * Simplification and Reducing Fractions
  *
  * @author Aron D. Bishop
  * @since February 3, 2020
@@ -57,41 +57,12 @@ public class Fraction {
     }
 
     /**
-     *                                                  \\//ReduceFraction\\//
-     *
-     *Reduces the fraction(numerator & denominator) passed to method. The Reduce Fraction method is based on
-     * Euclid’s Algorithm.
-     *
-     * Formula:
-     * 1)find the greatest common divisor(GCD) of two integers 𝑚 and 𝑛
-     *
-     * 2) Test to see if 𝑛 divides 𝑚 evenly- if It does then the ANSWER: GCD is n
-     * 2) if 𝑛 does not divide 𝑚 evenly- then the ANSWER: is the GCD of 𝑛 and the remainder of 𝑚 divided by 𝑛
-     *
-     * 3) Done
-     *
-     * @param f1 passes fraction object to the reduceFraction method it contains the simplified fractions, which is a
-     * numerator and denominator
-     * @return returns the simplified fraction(numerator & denominator) as an integer number
-     */
-
-    public int gcd(Fraction f1) {
-
-      int m= f1.denominator;
-      int n= f1.numerator;
-        while (m % n != 0) {
-            int oldm = m;
-            int oldn = n;
-
-            m = oldn;
-            n = oldm % oldn;
-        }
-        return n;
-    }
-
-    /**
      *                                                  \\//Simplification\\//
      *
+     *The simplification method modifies the fractions numerator and denominator so that there is no negative on
+     * denominator. A negative numerator is acceptable. This method is utilized to find the GCD using Euclid's formula.
+     * Euclid's cannot have a negative in denominator to work correct. Hence, this method places the negative in the
+     * numerator.
      *
      *
      * @param f1 Passes a fraction of the users choice to simplification method.
@@ -114,9 +85,20 @@ public class Fraction {
     /**
      *                                                   \\//Addition\\//
      *
-     * @param f1 The first fraction- It includes a numerator & denominator
-     * @param f2 The second fraction- It includes a numerator & denominator
-     * @return Returns a new Fraction object with fraction addition operation completed
+     * uses the mathematical formula for fraction addition to find the new fraction after completing the formula.
+     * To utilize the gcd method it requires simplification method to ensure no negative is on the denominator.
+     *
+     * Formula:
+     *
+     * 1) Multiply the denominator from fraction 1 and 2
+     * 2) Multiply the numerator of fraction 1 by denominator of fraction 2.
+     * 3) Multiply the numerator of fraction 2 by denominator of fraction 1.
+     * 4) add the numerator from step 2 and 3-This is the new numerator.
+     * 5)Done
+     *
+     * @param f1 The first fraction- It includes a numerator and denominator.
+     * @param f2 The second fraction- It includes a numerator and denominator.
+     * @return Returns a new Fraction object with fraction addition operation completed.
      */
         public static Fraction add(Fraction f1, Fraction f2) {
 
@@ -137,14 +119,23 @@ public class Fraction {
     /**
      *                                                 \\//Subtraction\\//
      *
-     * @param f1 The first fraction- It includes a numerator & denominator
-     * @param f2 The second fraction- It includes a numerator & denominator
-     * @return Returns a new Fraction object with fraction subtraction operation completed
+     * uses the mathematical formula for fraction subtraction to find the new fraction after completing the formula.
+     * To utilize the gcd method it requires simplification method to ensure no negative is on the denominator.
+     *
+     * Formula:
+     *
+     * 1) Multiply the denominator from fraction 1 and 2
+     * 2) Multiply the numerator of fraction 1 by denominator of fraction 2.
+     * 3) Multiply the numerator of fraction 2 by denominator of fraction 1.
+     * 4) subtract the numerator from step 2 and 3-This is the new numerator.
+     * 5)Done
+     *
+     * @param f1 The first fraction- It includes a numerator and denominator.
+     * @param f2 The second fraction- It includes a numerator and denominator.
+     * @return Returns a new Fraction object with fraction subtraction operation completed.
      */
+
         public static Fraction subtract(Fraction f1, Fraction f2) {
-
-
-
 
             int numerator1 = f1.numerator * f2.denominator;
 
@@ -163,11 +154,20 @@ public class Fraction {
 
     /**
      *                                                  \\//Multiply\\//
+     *uses the mathematical formula for fraction multiplication to find the new fraction after completing the formula.
+     * To utilize the gcd method it requires simplification method to ensure no negative is on the denominator.
      *
-     * @param f1 The first fraction- It includes a numerator & denominator
-     * @param f2 The second fraction- It includes a numerator & denominator
-     * @return Returns a new Fraction object with fraction multiplication operation completed
+     * Formula:
+     *
+     * 1) Multiply the numerator from fraction 1 and 2.
+     * 2)Multiply the denominator from fraction 1 and 2.
+     * 3)done.
+     *
+     * @param f1 The first fraction- It includes a numerator and denominator.
+     * @param f2 The second fraction- It includes a numerator and denominator.
+     * @return Returns a new Fraction object with fraction multiplication operation completed.
      */
+
     public static Fraction multiply(Fraction f1, Fraction f2) {
 
             int newFractionNumerator = f1.numerator *f2.numerator ;
@@ -184,19 +184,21 @@ public class Fraction {
 
     /**
      *                                                  \\//Divide\\//
+     *
      * uses the mathematical formula for fraction division to find the new fraction after completing the formula.
+     *To utilize the gcd method it requires simplification method to ensure no negative is on the denominator.
      *
      * Formula:
      *
-     * 1)Leave the first Fraction alone
-     * 2) get the inverse of second fraction(switch the numerator and denominator)
-     * 3)multiply inverse fractions numerator by first fraction numerator
-     * 4) multiply inverse fraction denominator by first fraction denominator
-     * 5)Done
+     * 1)Leave the first Fraction alone.
+     * 2) get the inverse of second fraction(switch the numerator and denominator).
+     * 3)multiply inverse fractions numerator by first fraction numerator.
+     * 4) multiply inverse fraction denominator by first fraction denominator.
+     * 5)Done.
      *
-     * @param f1 The first fraction- It includes a numerator & denominator
-     * @param f2 The second fraction- It includes a numerator & denominator
-     * @return Returns a new Fraction object with fraction division operation completed
+     * @param f1 The first fraction- It includes a numerator and denominator.
+     * @param f2 The second fraction- It includes a numerator and denominator.
+     * @return Returns a new Fraction object with fraction division operation completed.
      */
         public static Fraction divide(Fraction f1, Fraction f2) {
 
@@ -216,13 +218,47 @@ public class Fraction {
         }
 
     /**
+     *                                                  \\//ReduceFraction\\//
+     *
+     *Reduces the fraction(numerator and denominator) passed to method. The Reduce Fraction method is based on
+     * Euclid’s Algorithm.
+     *
+     * Formula:
+     * 1)find the greatest common divisor(GCD) of two integers m and n
+     *
+     * 2) Test to see if n divides m evenly- if It does then the ANSWER: GCD is n
+     * 2) if n does not divide m evenly- then the ANSWER: is the GCD of n and the remainder of m divided by n
+     *
+     * 3) Done
+     *
+     * @param f1 passes fraction object to the reduceFraction method it contains the simplified fractions, which is a
+     * numerator and denominator
+     * @return returns the simplified fraction(numerator and denominator) as an integer number
+     */
+
+    public int gcd(Fraction f1) {
+
+      int m= f1.denominator;
+      int n= f1.numerator;
+        while (m % n != 0) {
+            int oldm = m;
+            int oldn = n;
+
+            m = oldn;
+            n = oldm % oldn;
+        }
+        return n;
+    }
+
+    /**
      *                                                  \\//toString\\///
      *
      * toString is a built in java method that prints out the memory address of the object. I have over written It to
-     * return a specified parameters
+     * return a specified parameters.
      *
-     * @return -returns the numerator & denominator as a string
+     * @return returns the numerator and denominator as a string.
      */
+
     public String toString() {
             return numerator +"/"+denominator;
         }
